3de4fe0efe6ca33de7454e40bfb28ec70e4ee001
Brian Donnoe <dunow33@hotmail.com>
2017-09-19 16:04:16 -0400

    module 8

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,3 +2,24 @@ var passport = require('passport');
 var User = require('../models/user');
 var config = require('../config');
 var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+
+var JwtOptions = {
+	jwtFromRequest: ExtractJwt.fromHeader('authorization'), 
+	secretOrKey: config.secret
+};
+
+var jwtLogin = new JwtStrategy(JwtOptions, function(payload, done){
+	User.findById(payload.sub, function(err, user){
+		if(err){
+			return done(err, false);
+		}
+		if(user) {
+			done(null, user);
+		} else {
+			done(null, false);
+		}
+	})
+});
+
+passport.use(jwtLogin);
