c2e4bf2f7a5f8e70c08e4535fd3e2bc4bd77f0c0
Brian Donnoe <dunow33@hotmail.com>
2017-09-20 18:03:05 -0400

    Module 11.1

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import NavBarHeader from './nav';
 
-	export default class App extends Component {
-		render() {
-			return (
-					<div>React Hello World!!</div>
-				);
-		}
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<NavBarHeader />
+			</div>
+		);
 	}
+}
