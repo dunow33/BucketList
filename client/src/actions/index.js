188c4334397ebc4a0809d28c84050343426b76ae
Brian Donnoe <dunow33@hotmail.com>
2017-09-22 11:46:22 -0400

    module 17 changes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, UNAUTH_USER } from './types';
+import { 
+  AUTH_USER,
+  UNAUTH_USER,
+  AUTH_ERROR 
+ } from './types';
 import authReducer from '../reducers/auth_reducer';
 
 
@@ -11,15 +15,22 @@ const ROOT_URL = 'http://localhost:3000';
 export function signinUser({ email, password }){
 	return function(dispatch){
 		axios.post(`${ROOT_URL}/signin`, {email, password})
- 		.then(response => {
- 			dispatch({ type: AUTH_USER });
- 				
-			localStorage.setItem('token', response.data.token);
-			browserHistory.push('/newitem');
-		 })
-      		.catch(() => {
-         });
+ 			.then(response => {
+ 		
+ 				dispatch({ type: AUTH_USER });
+ 				localStorage.setItem('token', response.data.token);
+ 				browserHistory.push('/newitem');
+      	
+      	 })
+      		.catch(response =>  dispatch(authError("Bad login info")));
 	}
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
 
 export function createPost(props) {
