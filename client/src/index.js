b15390b6d6d89d1d2e03b565fa7b0bd97c5a0a49
Brian Donnoe <dunow33@hotmail.com>
2017-09-22 10:03:26 -0400

    module 16

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
+import ListItem from './components/list/new-list-item';
 import reducers from './reducers';
 
-var createStoreWithMiddleware = applyMiddleware()(createStore);
+var createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
 		<Router history={browserHistory}>
   			<Route path="/" component={App}>
   			  	<Route path="signin" component={Signin} />
+  			  	<Route path="newitem" component={ListItem} />
   			</Route>
   		</Router>
   </Provider>
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var http = require('http');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');		
 var app = express();
 var router = require('./router');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
-
-mongoose.connect('mongodb://localhost:bucket/bucket');
-app.use(bodyParser.json({type: '*/*'}));
+//DB Connection
+mongoose.connect('mongodb://localhost:bucketlist/bucket');
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*'}));
 router(app);
 
 var port = process.env.PORT || 3000;
@@ -15,4 +17,4 @@ var port = process.env.PORT || 3000;
 var server = http.createServer(app);
 
 server.listen(port);
-console.log('Server is listening on ' + port);
+console.log('Server listening on ' + port);
