04d7150231db4774c6dab7c2864742d3be3a01bd
Brian Donnoe <dunow33@hotmail.com>
2017-09-22 17:04:06 -0400

    module 21 bucket list model

--- server/router.js
+++ server/router.js
@@ -1,4 +1,6 @@
 var Auth = require('./controllers/auth');
+var BucketList = require('./controllers/bucketlistcontroller');
+
 var passportService = require('./services/passport');
 var passport = require('passport');
 
@@ -6,11 +8,8 @@ var requireAuth = passport.authenticate('jwt', {session: false});
 var requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
-	app.get('/', requireAuth, function(req, res){
-		res.send('Hello Homepage');
-	});
-
+	
 	app.post('/signup', Auth.signup);
-
 	app.post('/signin', requireSignin, Auth.signin);
+	app.post('/newitem', requireAuth, BucketList.addBucketList);
 }
